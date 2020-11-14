b172e9d8b42dc285a3b6ae2c94aae6d27c9291c7
NurAminSifatTanha <nur15-1463@diu.edu.bd>
2020-11-14 14:36:35 +0600

    checkout page and popup upgrade

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="home"),
+    path('', views.index,name="shopHome"),
     path('about/', views.about,name="AboutUs"),
     path('contact/', views.contact,name="ContactUs"),
     path('tracker/', views.tracker,name="TrackingStatus"),
     path('search/', views.search,name="Search"),
-    path('productView/', views.prodView,name="productView"),
+    path('products/<int:id>/', views.prodView,name="productView"),
     path('checkout/', views.checkout,name="Checkout"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import *
 from math import ceil
-
+from django.shortcuts import get_object_or_404
 
 def index(request):
     # products = Product.objects.all()
@@ -26,26 +26,37 @@ def index(request):
     return render(request, 'shop/index.html', params)
 
 
-
 def about(request):
     return render(request, 'shop/about.html')
 
 
 def contact(request):
-    return HttpResponse("we are at contact")
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        # print(name,email,phone,desc)
+        contact = Contact.objects.create(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html')
 
 
 def tracker(request):
-    return HttpResponse("we are at tracker")
+    return render(request, 'shop/tracker.html')
 
 
 def search(request):
-    return HttpResponse("we are at search")
+    return render(request, 'shop/search.html')
 
 
-def prodView(request):
-    return HttpResponse("we are at prodView")
+def prodView(request,id):
+    #fetch the product using id
+    product = Product.objects.get(id=id)
+    # product = get_object_or_404(Product,id=id)
+    print(product)
+    return render(request, 'shop/prodView.html',{'product':product})
 
 
 def checkout(request):
-    return HttpResponse("we are at checkout")
+    return render(request, 'shop/checkout.html')
